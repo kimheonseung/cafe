956cf01841b3f4aa6a7428e16db121c4b9159593
kimheonseung <khs920210@gmail.com>
2023-09-15 00:29:24 +0900

    [CAFE-1] Feat: 프로젝트 초기 세팅

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+rootProject.name = "cafe"
+
+pluginManagement {
+    val kotlinVersion = "1.8.22"
+    val springBootVersion = "3.1.3"
+    val springDependencyManagementVersion = "1.0.11.RELEASE"
+
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
+                "org.jetbrains.kotlin.kapt" -> useVersion(kotlinVersion)
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+            }
+        }
+    }
+}
