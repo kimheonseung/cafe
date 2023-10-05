c78eab122ba3e2d316d839b5ca18f92c5ec836cf
kimheonseung <khs920210@gmail.com>
2023-10-05 23:24:37 +0900

    [CAFE-8] Feat: api 공통 기능 작성을 위한 종속성 추가 및 jar enable

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -3,8 +3,13 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
 
-jar.enabled = false
+jar.enabled = true
 bootJar.enabled = false
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
 
 subprojects {
     dependencies {
