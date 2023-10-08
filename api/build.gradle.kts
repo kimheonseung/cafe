4809ba7fdd615085d2f08520242b73ab46916575
kimheonseung <khs920210@gmail.com>
2023-10-08 19:37:29 +0900

    [REFACTOR-5] Feat: 공통 에러 응답 추가 및 유효성 검사 관련 핸들러 추가

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -8,6 +8,7 @@ bootJar.enabled = false
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
