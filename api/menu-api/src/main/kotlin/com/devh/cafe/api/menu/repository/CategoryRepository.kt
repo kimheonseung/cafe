607090551e42bc928743e3ec454ea16dd694a3b8
kimheonseung <khs920210@gmail.com>
2023-10-29 23:02:40 +0900

    [CAFE-15] Feat: 하위 카테고리 및 재귀 조회 기능 repository 작성

--- api/menu-api/src/main/kotlin/com/devh/cafe/api/menu/repository/CategoryRepository.kt
+++ api/menu-api/src/main/kotlin/com/devh/cafe/api/menu/repository/CategoryRepository.kt
@@ -1,7 +1,11 @@
 package com.devh.cafe.api.menu.repository
 
 import com.devh.cafe.infrastructure.database.entity.Category
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import java.util.Optional
 
@@ -9,4 +13,33 @@ import java.util.Optional
 interface CategoryRepository : JpaRepository<Category, Long> {
     fun findByName(name: String): Optional<Category>
     fun findAllByNameIn(names: MutableList<String>): MutableList<Category>
+    fun findAllByParent(parent: Category, pageable: Pageable): Page<Category>
+    @Query(
+        nativeQuery = true,
+        value = """
+            with recursive cte (id, name, parent) as (
+            select id, name, parent from category where id = :id
+            union all
+            select child.id, child.name, child.parent 
+            from category child 
+            inner join cte parent on child.parent = parent.id
+            )
+            select id, name, parent from cte
+        """
+    )
+    fun findSubCategoryNamesRecursiveById(@Param(value = "id") id: Long): MutableSet<Category>
+    @Query(
+        nativeQuery = true,
+        value = """
+            with recursive cte (id, name, parent) as (
+            select id, name, parent from category where name = :name
+            union all
+            select child.id, child.name, child.parent 
+            from category child 
+            inner join cte parent on child.parent = parent.id
+            )
+            select id, name, parent from cte
+        """
+    )
+    fun findSubCategoryNamesRecursiveByName(@Param(value = "name") name: String): MutableSet<Category>
 }
