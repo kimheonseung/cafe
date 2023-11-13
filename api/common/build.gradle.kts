04643fe6a6e0f0cdc689b9ad45adf78b9fd55cfd
kimheonseung <khs920210@gmail.com>
2023-11-13 15:47:55 +0900

    [REFACTOR-11] Refactor: 모듈 정리

--- api/common/build.gradle.kts
+++ api/common/build.gradle.kts
@@ -3,5 +3,5 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 val jar: Jar by tasks
 val bootJar: BootJar by tasks
 
+jar.enabled = true
 bootJar.enabled = false
-jar.enabled = false
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,5 @@
 rootProject.name = "cafe"
-include("infrastructure")
-include("infrastructure:database")
-findProject(":infrastructure:database")?.name = "database"
-include("utility")
-include("api")
-include("api:menu-api")
-findProject(":api:menu-api")?.name = "menu-api"
-
-pluginManagement {
-    val kotlinVersion = "1.8.22"
-    val springBootVersion = "3.1.3"
-    val springDependencyManagementVersion = "1.0.11.RELEASE"
-
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
-                "org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
-                "org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
-                "org.jetbrains.kotlin.kapt" -> useVersion(kotlinVersion)
-                "org.springframework.boot" -> useVersion(springBootVersion)
-                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
-            }
-        }
-    }
-}
+include(":infrastructure:database")
+include(":utility")
+include(":api:common")
+include(":api:menu-api")
