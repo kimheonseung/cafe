e5c94df6b2109611bafa18f39193ec68a4f3551c
kimheonseung <khs920210@gmail.com>
2024-01-01 22:30:02 +0900

    [REFACTOR-12] Feat: kotlin-loggin, kotest로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,19 +58,22 @@ subprojects {
 		implementation("org.springframework.boot:spring-boot-starter")
 		implementation("org.springframework.boot:spring-boot-starter-parent:3.1.5")
 
-		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-		implementation("org.springframework.boot:spring-boot-starter-test")
 
+		/* test */
+		implementation("org.springframework.boot:spring-boot-starter-test")
+		testImplementation("io.kotest:kotest-runner-junit5:5.+")
+		testImplementation("io.kotest:kotest-assertions-core:5.+")
+		testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.+")
+
+		/* JPA */
+		implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 		implementation("com.querydsl:querydsl-jpa:${querydslVersion}:jakarta")
 		kapt("com.querydsl:querydsl-apt:${querydslVersion}:jakarta")
 		annotationProcessor("jakarta.annotation:jakarta.annotation-api")
 		annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 
-		implementation("org.apache.logging.log4j:log4j-api")
-		implementation("org.apache.logging.log4j:log4j-core")
-		implementation("org.apache.logging.log4j:log4j-to-slf4j")
-
-		implementation("org.apache.commons:commons-lang3")
+		/* logging */
+		implementation("io.github.microutils:kotlin-logging:3.+")
 	}
 }
 
